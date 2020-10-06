280b5f76f738d2ace1851333056ff300964970c1
lmacintosh <lupe.macintosh@gmail.com>
2020-10-06 09:40:41 -0700

    added sql integration

--- bookScraper.py
+++ bookScraper.py
@@ -1,4 +1,8 @@
 import requests
+import sqlite3
+
+conn = sqlite3.connect('overduepod.db') #setting up access to sqlite db
+c = conn.cursor() #make sure to end with and conn.commit() and conn.close()
 
 #the https response is a dictionary, the items value is a list of books, each book is a dictionary
 
@@ -20,6 +24,7 @@ def getBookID(title, author):
         bookID=0
     return bookID
  
+#helper function that formats a query string 
 def makeQueryStr(title, author):
     cleanTitle=title.lower()
     cleanAuthor=author.lower()
@@ -36,6 +41,20 @@ def getBookInfo(bookID):
     genres = volumeInfo["categories"]
     return [pageCount, genres]
 
+#function that cycles through the epList to identify books
+def epCycle():
+    for n in range(450):
+        c.execute("SELECT title, author FROM epList WHERE epNumber =?", n)
+        c.fetchone()
+
+#function that adds a record to the book database
+
+def addBook(title, author):
+    bookID = getBookID(title, author)
+    c.execute("INSERT INTO bookList VALUES (?, ?, ?)", (title, author, bookID))
+
+#function that adds other book into the book database
+
 #Function that updates read info for a given book (unclear what the reference will be, or whether that should be in this file)
 
 """ response = requests.get("https://www.googleapis.com/books/v1/volumes?q=Beowulf+inauthor:seamus+heaney")
@@ -46,6 +65,11 @@ topBook = items[1]
 bookID = topBook["id"]
 print("Direct link is "+topBook["selfLink"]+" and the id is "+bookID) """
 
-bookID = getBookID("Portnoy's Complaint", "Philip Roth")
-print(getBookInfo(bookID))
+testTitle = "Portnoy's Complaint"
+testAuthor = "Philip Roth"
 
+#bookID = getBookID(testTitle, testAuthor)
+#print(getBookInfo(bookID))
+addBook(testTitle, testAuthor)
+
+conn.close()
